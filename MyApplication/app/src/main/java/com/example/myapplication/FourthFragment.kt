1247eda4255c52597066a6669c9eb516db4848bb
hakoshiki <noah9no9ark@gmail.com>
2020-03-05 18:38:49 +0900

    GUIでForthFragment足してみて、3と4をNestにしてみた

--- MyApplication/app/src/main/java/com/example/myapplication/FourthFragment.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/FourthFragment.kt
@@ -1,21 +1,20 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_first.view.*
 
-class ThirdFragment : Fragment() {
+class FourthFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_third, container, false)
 
         view.button.setOnClickListener {
-            findNavController().navigate(R.id.action_thirdFragment_to_fourthFragment)
-//            findNavController().popBackStack(R.id.firstFragment, false) //スタック積み続けるのを避けるにはこちら
+            //            findNavController().navigate(R.id.action_third_to_first)
+            findNavController().popBackStack(R.id.firstFragment, false) //スタック積み続けるのを避けるにはこちら
         }
 
         return view
