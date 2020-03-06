b59577731df92fa6f9ed4a36e038b05eb456840c
hakoshiki <noah9no9ark@gmail.com>
2020-03-06 16:27:08 +0900

    外部ファイル化してinclude

--- MyApplication/app/src/main/java/com/example/myapplication/FourthFragment.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/FourthFragment.kt
@@ -10,7 +10,7 @@ import kotlinx.android.synthetic.main.fragment_first.view.*
 
 class FourthFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_third, container, false)
+        val view = inflater.inflate(R.layout.fragment_fourth, container, false)
 
         view.button.setOnClickListener {
             //            findNavController().navigate(R.id.action_third_to_first)
